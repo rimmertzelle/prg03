f50523bc2bf3d025e4322118ef43f346accd4003
Kokodoko <benni_jolink@hotmail.com>
2016-02-09 14:14:12 +0100

    Oefeningen week 2 toegevoegd

---
+++ week2/dom2/dom2_start.js
@@ -0,0 +1,6 @@
+// OPDRACHT
+
+// 1 MAAK EEN FUNCTIE addItem 
+// deze wordt aangeroepen door de button in de html
+
+// 2 De functie moet een li item toevoegen aan de bestaande UL
---
+++ week2/dom3/dom3_start.js
@@ -0,0 +1,4 @@
+// OPDRACHT
+
+// de functie addItem moet nu kijken wat er in het formulier is ingevuld
+// deze waarde moet toegevoegd worden aan de todo list

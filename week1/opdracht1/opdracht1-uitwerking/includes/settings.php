a4fd19f7ced25a82821f99ec9788edb9015e925a
Rimmert Zelle <rimmert.zelle@gmail.com>
2016-02-16 11:33:33 +0100

    uitwerking opdracht 1
    Uitwerking opdracht 1

---
+++ week1/opdracht1/opdracht1-uitwerking/includes/settings.php
@@ -0,0 +1,11 @@
+<?php
+//Database
+mysqli_report(MYSQLI_REPORT_STRICT); // Make sure we can catch errors
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_DATABASE", "dishes-for-students");
+
+//Flickr
+define("FLICKR_KEY", "YOUR_KEY");
+define("FLICKR_SECRET", "YOUR_SECRET");
